8fbd042d412a6f243af454dae69d24b0e07f5397
Michael Wixson <wixsonmc@gmail.com>
2020-02-06 18:43:34 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,19 +32,19 @@
 
 def naming_system
   
-{label: "Kingdom",
+    {label: "Kingdom",
     sub_category: {
         label: "Phylum",
         sub_category: {
           label: "Class",
           sub_category: {
-            label: "Order"
+            label: "Order",
             sub_category: {
               label: "Family",
               sub_category: {
                 label: "Genus",
                 sub_category: {
-                  label: "Species"
+                  label: "Species",
                   sub_category: nil
                 }
               
